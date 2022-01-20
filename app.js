1026d7aa7dd374cc270d3db2512e2d83f1a05c1d
SubHeaven <subheaven.paulo@gmail.com>
2022-01-20 17:14:05 -0300

    revertendo commit errado

--- app.js
+++ app.js
@@ -6,16 +6,13 @@ const cookieParser = require('cookie-parser');
 
 const env = require('subheaven-env');
 env.addParams([
-    { name: 'PORT', description: 'Porta onde o sistema irá rodar.', required: false, sample: '33320' },
-    { name: 'SERVICE', description: 'Caminho onde está instalado o Dharma Serviços.', required: false, sample: 'C:\\iacon\\dharma-servicos' },
-    { name: 'NAME', description: 'Nome da lista de tarefas', required: false, sample: 'malha_antecipada' },
+    { name: 'PORT', description: 'Porta onde o sistema irá rodar.', required: false, sample: '33320' }
 ]);
 env.config();
 
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const tasks = require('./routes/tasks');
 
 const app = express();
 
@@ -28,6 +25,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/tasks', tasks);
 
 module.exports = app;
